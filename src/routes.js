6320066b14c90d5b4d0121b46a839f55b548272c
Everton Dias <esrdias@gmail.com>
2021-01-04 07:05:00 -0300

    Criando rotas para lidar com as playlists

--- src/routes.js
+++ src/routes.js
@@ -71,4 +71,68 @@ routes.delete('/music/:id', (request, response) => {
 	return response.json({ message: 'Musica deletada' })
 });
 
+// ********
+// Playlist
+// ********
+routes.get('/playlist/:id', (request, response, next) => {
+
+	let id = request.params.id;
+
+	db.all("SELECT * FROM playlist_song WHERE playlist_id = ?", [id], (error, value) => {
+		if (error) {
+			throw error;
+		}
+
+		return response.json(value)
+	});
+});
+
+
+// Criar playlist
+routes.post('/playlist/create', (request, response) => {
+	let query = `INSERT INTO playlists ('name', 'description', 'image')
+	VALUES ('${request.body.name}', '${request.body.description}', '${request.body.image}')`;
+	
+	db.run(query);
+
+	return response.json({ message: 'Okay, playlist criada com sucesso!' })
+});
+
+// Adiciona musica a uma playlist
+routes.post('/add-song-playlist/:playlist_id/:song_id', (request, response) => {
+	let query = `INSERT INTO playlist_song ('playlist_id', 'song_id')
+	VALUES ('${request.params.playlist_id}', '${request.params.song_id}')`;
+
+	db.run(query);
+
+	return response.json({ message: 'Okay' })
+});
+
+// Remove musica de uma playlist
+routes.delete('/remove-song-playlist/:playlist_id/:song_id', (request, response) => {
+	let query = `DELETE FROM playlist_song
+	WHERE playlist_id = ${request.params.playlist_id} AND song_id = ${request.params.song_id}`;
+
+	db.run(query);
+
+	return response.json({ message: 'Okay' })
+});
+
+// Lista todas as playlist
+routes.get('/all-playlists', (request, response) => {
+	db.all("SELECT * FROM playlists", (error, value) => {
+		if (error) {
+			throw error;
+		}
+
+		return response.json(value)
+	});
+});
+
+routes.delete('/delete-playlist/:id', (request, response) => {
+	db.run(`DELETE FROM playlists WHERE id = ${request.params.id}`);
+
+	return response.json({ message: 'Playlist deletada' })
+});
+
 module.exports = routes;
